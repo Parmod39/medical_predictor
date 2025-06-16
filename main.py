b63a1bde54ab0462054e0156806600f1cfd70edc
Parmod Budhiraja <budhirajaparmod@gmail.com>
2025-06-16 21:00:01 +0530

    Start script: connects UI and prediction logic

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ from pywebio import start_server
 if __name__ == '__main__':
     app = DiseasePredictorApp(DecisionTreeStrategy)
     start_server(app.run, port=8082, debug=True)# updated to refresh commit label
+# updated to refresh commit label
